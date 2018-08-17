62827af179e07b630593c99a7139166e621b0622
Jumpei Matsuda <jmatsu.drm@gmail.com>
2018-08-17 15:41:54 +0900

    Implemented minimum upload feature

--- step.sh
+++ step.sh
@@ -6,15 +6,31 @@ echo "$api_key"
 echo "$owner_name"
 echo "$app_path"
 
-# curl | envman add --key DEPLOYGATE_UPLOAD_APP_STEP_RESULT_JSON
-# Envman can handle piped inputs, which is useful if the text you want to
-# share is complex and you don't want to deal with proper bash escaping:
-#  cat file_with_complex_input | envman add --KEY EXAMPLE_STEP_OUTPUT
-# You can find more usage examples on envman's GitHub page
-#  at: https://github.com/bitrise-io/envman
+upload_app() {
+  set +e
 
-#
-# --- Exit codes:
-# The exit code of your Step is very important. If you return
-#  with a 0 exit code `bitrise` will register your Step as "successful".
-# Any non zero exit code will be registered as "failed" by `bitrise`.
+  curl -# -X POST \
+    -H "Authorization: token $api_key" \
+    -F "file=@$app_path" \
+    "https://deploygate.com/api/users/$owner_name/apps" 
+
+  set -e
+  return 0
+}
+
+parse_error_field() {
+  cat - | ruby -rjson -ne 'puts JSON.parse($_)["error"]'
+}
+
+upload_app > output.json
+envman add --key DEPLOYGATE_UPLOAD_APP_STEP_RESULT_JSON < output.json
+
+if [[ "$(cat output.json | parse_error_field)" == "false" ]]; then
+  # upload successfully
+  cat output.json
+  exit 0
+else
+  # WTF
+  cat output.json
+  exit 1
+fi
