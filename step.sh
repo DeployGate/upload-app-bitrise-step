aa3cb0a1634924f22233d64210b25151827a02e2
Jumpei Matsuda <jmatsu.drm@gmail.com>
2018-08-17 15:18:27 +0900

    Added input values and confirmed it worked

--- step.sh
+++ step.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
+
 set -ex
 
-echo "This is the value specified for the input 'example_step_input': ${example_step_input}"
+echo "$api_key"
+echo "$owner_name"
+echo "$app_path"
 
-#
-# --- Export Environment Variables for other Steps:
-# You can export Environment Variables for other Steps with
-#  envman, which is automatically installed by `bitrise setup`.
-# A very simple example:
-envman add --key EXAMPLE_STEP_OUTPUT --value 'the value you want to share'
+# curl | envman add --key DEPLOYGATE_UPLOAD_APP_STEP_RESULT_JSON
 # Envman can handle piped inputs, which is useful if the text you want to
 # share is complex and you don't want to deal with proper bash escaping:
 #  cat file_with_complex_input | envman add --KEY EXAMPLE_STEP_OUTPUT
